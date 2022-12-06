bea1606bb051d9527c1c728fd45aed44d3ed4ede
Mattia Gallotta <mattia.gallotta@studenti.unimi.it>
2022-12-06 16:36:13 +0100

    Removed useless view WelcomeView

--- app/handmania/ContentView.swift
+++ app/handmania/ContentView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var appName: String {
+        get {
+            // The app name is set as an environment variable.
+            guard let appName = ProcessInfo.processInfo.environment["APP_NAME"] else {
+                fatalError("unspecified app name")
+            }
+            
+            return appName
+        }
+    }
+    
     @StateObject private var cameraManager = CameraManager.getInstance()
     @State private var permissionGranted = false
     @StateObject private var handDirectionManager = HandDirectionsManager.getInstance()
@@ -17,21 +28,37 @@ struct ContentView: View {
     
     var body: some View {
         VStack (alignment: .center) {
-            WelcomeView(songsHaveArrived: model.songsHaveArrived)
+            NavigationStack {
+                VStack {
+                    Image("Logo")
+                        .resizable()
+                        .frame(width: 256.0, height: 256.0)
+                    Text(appName)
+                        .font(.system(size: 36))
+                        .padding()
+                    Text("Progetto di \"Sviluppo di Applicazioni per Dispositivi Mobili\"")
+                        .font(.system(size: 20))
+                    Text("Gallotta, Santise")
+                        .font(.system(size: 20))
+                    
+                    if model.songsHaveArrived {
+                        NavigationLink("Inizia a giocare!", destination: SongsView(songs: Model.getInstace().songs))
+                            .padding()
+                    }
+                }
+            }
             
-            /*
-             if cameraManager.permissionGranted {
-             ZStack {
-             AVCaptureVideoPreviewLayerAdapter(session: handDirectionManager.captureSession)
-             
-             if let directionBoxes = handDirectionManager.directionBoxes?.values {
-             DirectionBoxesView(directionBoxes: Array(directionBoxes))
-             }
-             }
-             } else {
-             Text("Per utilizzare l'applicazione devi fornire i permessi della camera")
-             }
-             */
+            /*if cameraManager.permissionGranted {
+                ZStack {
+                    AVCaptureVideoPreviewLayerAdapter(session: handDirectionManager.captureSession)
+                    
+                    if let directionBoxes = handDirectionManager.directionBoxes?.values {
+                        DirectionBoxesView(directionBoxes: Array(directionBoxes))
+                    }
+                }
+            } else {
+                Text("Per utilizzare l'applicazione devi fornire i permessi della camera")
+            }*/
         }.onReceive(cameraManager.$permissionGranted, perform: { granted in
             permissionGranted = granted
         }).onAppear {
